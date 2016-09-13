6afe0d92964e446ce8398a155662283b5842ff58
huangziyi01 <huangziyi01@baidu.com>
2016-09-13 22:47:33 +0800

    发布组件

---
+++ lib/image-zoom/image-zoom.component.d.ts
@@ -0,0 +1,7 @@
+import * as React from 'react';
+import * as typings from './image-zoom.type';
+export default class ImageZoom extends React.Component<typings.PropsDefine, typings.StateDefine> {
+    static defaultProps: typings.PropsDefine;
+    state: typings.StateDefine;
+    render(): JSX.Element;
+}
