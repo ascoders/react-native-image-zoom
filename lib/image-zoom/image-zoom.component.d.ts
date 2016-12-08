edf0441ceba6470a45eabb61f4139a72592ab4ff
huangziyi01 <huangziyi01@baidu.com>
2016-12-08 18:31:52 +0800

    发布组件

---
+++ lib/image-zoom/image-zoom.component.d.ts
@@ -0,0 +1,32 @@
+import * as React from 'react';
+import * as typings from './image-zoom.type';
+export default class ImageViewer extends React.Component<typings.PropsDefine, typings.StateDefine> {
+    static defaultProps: typings.PropsDefine;
+    state: typings.StateDefine;
+    private lastPositionX;
+    private positionX;
+    private animatedPositionX;
+    private lastPositionY;
+    private positionY;
+    private animatedPositionY;
+    private scale;
+    private animatedScale;
+    private zoomLastDistance;
+    private zoomCurrentDistance;
+    private imagePanResponder;
+    private lastTouchStartTime;
+    private horizontalWholeOuterCounter;
+    private horizontalWholeCounter;
+    private verticalWholeCounter;
+    private centerDiffX;
+    private centerDiffY;
+    private longPressTimeout;
+    private lastClickTime;
+    private doubleClickX;
+    private doubleClickY;
+    private isDoubleClickScale;
+    componentWillMount(): void;
+    handleLayout(event: React.LayoutChangeEvent): void;
+    reset(): void;
+    render(): JSX.Element;
+}
