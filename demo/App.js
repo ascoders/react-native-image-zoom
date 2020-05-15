0175ef47a9ffd3d734c898a51d3e6dad01db2acb
Artem Kolichenkov <artem@kolichenkov.com>
2020-05-16 00:23:32 +0300

    Update demo app
    Added debuggin page to test handlers

--- demo/App.js
+++ demo/App.js
@@ -4,6 +4,7 @@ import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 import HomeScreen from './HomeScreen';
 import BigImage from './BigImage';
+import DebugImage from './DebugImage';
 
 const Stack = createStackNavigator();
 
@@ -14,6 +15,7 @@ export default class App extends React.Component {
         <Stack.Navigator initialRouteName="Home">
           <Stack.Screen name="Home" component={HomeScreen} options={{ title: 'Rereact-native-image-pan-zoom' }} />
           <Stack.Screen name="BigImage" component={BigImage} options={{ title: 'Big Image' }} />
+          <Stack.Screen name="DebugImage" component={DebugImage} options={{ title: 'Debug Image' }} />
         </Stack.Navigator>
       </NavigationContainer>
     );
